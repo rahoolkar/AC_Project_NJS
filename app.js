2f92faf5b696edfcb531223ea9ae1b06e2f88667
rahoolkar <batman30jan2002@gmail.com>
2025-03-04 23:28:17 +0530

    create route

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ main().then(()=>{
     console.log(error);
 })
 
+app.use(express.json());
 app.use(express.urlencoded({extended : true}))
 app.set("view engine","ejs");
 app.set("views",Path.join(__dirname,"/views"));
@@ -28,10 +29,23 @@ app.get("/listings",async (req,res)=>{
     res.render("listings/index.ejs",{allListings});
 });
 
+//New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
+
 //Show Route
 app.get("/listings/:id",async (req,res)=>{
     let {id} = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
+})
+
+//Post Route
+app.post("/listings",async (req,res)=>{
+    let {title,description,price,image="https://www.shutterstock.com/image-vector/image-icon-trendy-flat-style-600nw-643080895.jpg",country,location} = req.body;
+    let newListing = new Listing({title,description,image,price,country,location});
+    await newListing.save();
+    res.redirect("/listings");
 })
 
