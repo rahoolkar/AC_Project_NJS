7b64e04a25f8ad176c7398178f0e7a524725bb77
rahoolkar <batman30jan2002@gmail.com>
2025-03-04 18:37:33 +0530

    basic set up

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const Listing = require("./models/listings");
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/project');
+}
+
+main().then(()=>{
+    console.log("connected to the database");
+    app.listen(3000,()=>{
+        console.log("app is running on port 3000");
+    })  
+}).catch((error)=>{
+    console.log(error);
+})
+
+app.get("/testListing",async (req,res)=>{
+    let newListing = new Listing({
+        title : "title",
+        description : "description",
+        price : 7800,
+        country : "India",
+        location : "Delhi"
+    })
+
+    await newListing.save();
+    res.send("Listing was saved successfully");
+})
+
+
+
---
+++ models/listings.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const listingSchema = new Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    image : {
+        type : String,
+        default : "https://www.shutterstock.com/image-vector/image-icon-trendy-flat-style-600nw-643080895.jpg",
+        set : (value)=> value ==="" ? "https://www.shutterstock.com/image-vector/image-icon-trendy-flat-style-600nw-643080895.jpg" : value,
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    location : {
+        type : String,
+        required : true
+    },
+    country : {
+        type : String,
+        required : true
+    }
+})
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
