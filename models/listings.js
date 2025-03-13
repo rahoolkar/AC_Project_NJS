dd3102647a0b426c88c06481a2e7b396d10819dd
rahoolkar <batman30jan2002@gmail.com>
2025-03-13 10:40:09 +0530

    creating review model

--- models/listings.js
+++ models/listings.js
@@ -1,3 +1,4 @@
+const { ref } = require("joi");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
@@ -26,7 +27,13 @@ const listingSchema = new Schema({
     country : {
         type : String,
         required : true
-    }
+    },
+    reviews : [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Review"
+        }
+    ]
 })
 
 const Listing = mongoose.model("Listing",listingSchema);
