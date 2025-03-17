bcb553fd97762a78c4d959a08ea19f54e993db51
rahoolkar <batman30jan2002@gmail.com>
2025-03-18 04:57:22 +0530

    login user

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,19 @@ const express = require("express");
 const router = express.Router();
 const wrapAysnc = require("../utils/wrapAsync");
 const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
 
-router.post("/signup",wrapAysnc(async (req, res) => {
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/signup",
+  wrapAysnc(async (req, res) => {
     try {
       let { username, email, password } = req.body;
       let newUser = new User({ username, email });
@@ -19,6 +26,18 @@ router.post("/signup",wrapAysnc(async (req, res) => {
       req.flash("failure", error.message);
       res.redirect("/signup");
     }
+  })
+);
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  wrapAysnc(async (req, res) => {
+    req.flash("success","hi, welcome to wanderlust");
+    res.redirect("/listings");
   })
 );
 
